62986a050218c6fe38ab8fcd744675fb54dd73c2
Erik Hedin <erik123567@gmail.com>
2015-12-06 20:04:21 -0500

    First Commit

---
+++ tournament.sql
@@ -0,0 +1,18 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+CREATE DATABASE tournament;
+\c tournament;
+
+CREATE TABLE players (id serial primary key, name varchar(30) not null);
+
+CREATE TABLE matches  ( id serial primary key,
+                        winner_id int,
+                        loser_id int,
+                        foreign key (winner_id) references players(id),
+                        foreign key (loser_id) references players(id));
